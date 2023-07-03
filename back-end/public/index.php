c1398580fda572322177838f08dbcf728410c69f
Stepans1 <78017597+Stepans1@users.noreply.github.com>
2023-07-03 22:11:34 +0300

    no message

--- back-end/public/index.php
+++ back-end/public/index.php
@@ -8,7 +8,7 @@ header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
 require_once __DIR__ . '/../vendor/autoload.php';
 //require_once('../vendor/autoload.php');
-const ROUTE = "/FINALA/back-end/public";
+
 
 //backend start
 $app=new Application();
